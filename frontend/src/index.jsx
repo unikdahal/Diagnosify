92cf1c0da6b3a9ac1b0a7c14a62b24dc55036df4
unikdahal <unikdahal03@gmail.com>
2024-03-18 02:09:51 +0530

    Router

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,12 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Landing from './Components/Landing';
+import DiagnosePage from './Components/Diagnose/DiagnosePage';
+import Error from './Components/Error';
+
+const AppLayout = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <Landing />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/diagnose/stage1",
+        element: <DiagnosePage />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/contact",
+        element: <DiagnosePage />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <DiagnosePage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(<RouterProvider router={appRouter}/>
 );
 
 
